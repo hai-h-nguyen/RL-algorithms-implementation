a4ab6b389ade29442f2a1c054799162c65c91b98
Hai Nguyen <32955308+hhn1n15@users.noreply.github.com>
2019-12-03 21:28:49 -0500

    Update spatial_autoencoder.py

--- autoencoder/spatial_autoencoder.py
+++ autoencoder/spatial_autoencoder.py
@@ -13,23 +13,12 @@ from torchvision.datasets import MNIST
 import os
 from spatial_softmax import SpatialSoftmax
 
-if not os.path.exists('./dc_img'):
-    os.mkdir('./dc_img')
-
-
-def to_img(x):
-    x = x.view(1, 240, 240)
-    return x
-
+if not os.path.exists('dc_img'):
+    os.mkdir('dc_img')
 
 num_epochs = 50
-batch_size = 128
+batch_size = 16
 learning_rate = 1e-3
-
-# img_transform = transforms.Compose([transforms.ToTensor(),
-#   transforms.Normalize((0.5,), (0.5,))
-# ])
-
 
 class autoencoder(nn.Module):
     def __init__(self):
@@ -37,30 +26,33 @@ class autoencoder(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 64, 7, stride=2),  # 3, 64, 117, 117
             nn.ReLU(True),
+            nn.BatchNorm2d(64),
             nn.Conv2d(64, 32, 5, stride=1),  # 3, 64, 117, 117
+            nn.BatchNorm2d(32),
             nn.ReLU(True),
             nn.Conv2d(32, 16, 5, stride=1),  # b, 8, 3, 3
+            nn.BatchNorm2d(16),
             nn.ReLU(True),
             SpatialSoftmax(109, 109, 16, temperature=None)
-
         )
 
 
         self.decoder = nn.Sequential(
-            nn.Linear(32, 57600),
-            nn.ReLU(True)
-        )
+            nn.Linear(32, 3600),
+            nn.Sigmoid()
+            )
 
 
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
+
         return x
 
 
 model = autoencoder().cuda()
 
-data_path = "~/Dropbox/ML-Projects/Methods/autoencoder"
+data_path = "images"
 train_dataset = torchvision.datasets.ImageFolder(
     root=data_path,
     transform=torchvision.transforms.ToTensor()
@@ -68,44 +60,46 @@ train_dataset = torchvision.datasets.ImageFolder(
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
-    batch_size=1,
+    batch_size=batch_size,
     num_workers=0,
     shuffle=True
 )
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
 
-transformations = transforms.Compose([transforms.ToPILImage(),
+transformations = transforms.Compose([transforms.ToPILImage(), transforms.Resize((60,60)),
                                           transforms.Grayscale(num_output_channels=1),
                                           transforms.ToTensor()])
 
 criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
-                             weight_decay=1e-5)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,
+                             momentum=0.9)
 
 for epoch in range(num_epochs):
     for data in train_loader:
         img, _ = data
         img = Variable(img).cuda()
+        # print(img.shape)
         # ===================forward=====================
         output = model(img)
 
-       
-        grey_tensor = transformations(img.cpu().view(3, 240, 240))
+        grey_out_image = torch.zeros([batch_size, 1, 60, 60], dtype=torch.float)
+        cnt = 0
+        for im in img:
+            grey_tensor = transformations(im.cpu().view(3, 240, 240))
+            grey_out_image[cnt] = grey_tensor
+            cnt += 1
 
-
-        # print(output.view(1, 60, 60).shape, grey_tensor.shape)
-        loss = criterion(output.view(1, 240, 240), grey_tensor.to(device))
-# #         # ===================backward====================
+        loss = criterion(output.view(-1, 1, 60, 60), grey_out_image.to(device))
+        # ===================backward====================
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-# #     # ===================log========================
+    # ===================log========================
     print('epoch [{}/{}], loss:{:.4f}'
           .format(epoch+1, num_epochs, loss.item()))
-    if epoch % 10 == 0 and epoch >0:
-        pic = to_img(output.cpu().data)
+    if epoch % 1 == 0:
+        pic = output[0].cpu().data.view(1, 60, 60)
         save_image(pic, './dc_img/image_{}.png'.format(epoch))
 
 torch.save(model.state_dict(), './conv_autoencoder.pth')
